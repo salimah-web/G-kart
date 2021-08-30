877414ae0682852cfb434ce22842f6c2af559368
Salima <salimaaomari@gmail.com>
2021-08-30 14:46:48 +0100

    #8

--- SWO/settings.py
+++ SWO/settings.py
@@ -149,7 +149,7 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS={
     messages.ERROR:'danger',
 }
-EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST='smtp.sendgrid.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER=env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD=env("EMAIL_HOST_PASSWORD")
