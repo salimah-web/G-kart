3150d81b6f5eab47d5a1fb17e85fefd03277ca34
Salima <salimaaomari@gmail.com>
2022-03-05 13:16:21 -0800

    Removed AWS configurations

--- SWO/settings.py
+++ SWO/settings.py
@@ -154,31 +154,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/assets/'
-# #STATIC_ROOT=os.path.join(BASE_DIR,'assets')
-# STATIC_ROOT=BASE_DIR/'static'
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'assets')]
+STATIC_URL = '/assets/'
+#STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATIC_ROOT=BASE_DIR/'static'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'assets')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=env("AWS_SECRET_KEY")
-AWS_STORAGE_BUCKET_NAME=env("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME)
-AWS_S3_OBJECT_PARAMETERS={
-    'CacheControl':'max-age=86400',
-}
+# AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY=env("AWS_SECRET_KEY")
+# AWS_STORAGE_BUCKET_NAME=env("AWS_STORAGE_BUCKET_NAME")
+# AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME)
+# AWS_S3_OBJECT_PARAMETERS={
+#     'CacheControl':'max-age=86400',
+# }
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'assets')]
-AWS_S3_FILE_OVERWRITE=False
-AWS_DEFAULT_ACL='public-read'
-AWS_LOCATION = "static"
-STATICFILES_STORAGE ='storages.backends.s3boto3.S3StaticStorage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-AWS_S3_REGION_NAME="us-west-2"
+# AWS_S3_FILE_OVERWRITE=False
+# AWS_DEFAULT_ACL='public-read'
+# AWS_LOCATION = "static"
+# STATICFILES_STORAGE ='storages.backends.s3boto3.S3StaticStorage'
+# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# AWS_S3_REGION_NAME="us-west-2"
 #AWS_QUERYSTRING_AUTH=False
 
-DEFAULT_FILE_STORAGE = 'SWO.media_storages.MediaStorage'
+#DEFAULT_FILE_STORAGE = 'SWO.media_storages.MediaStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
